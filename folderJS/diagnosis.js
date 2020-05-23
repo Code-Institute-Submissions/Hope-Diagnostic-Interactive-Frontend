f62b042a0ca6a728eab22872b3cebfddd7d8f92d
SegzAyo <jcide22@gmail.com>
2020-05-23 13:27:50 +0000

    API token call added to diagnosis.js

--- folderJS/diagnosis.js
+++ folderJS/diagnosis.js
@@ -2,6 +2,7 @@ var name = document.getElementById("")
 var yearOfBirth = document.getElementById("")
 var sex = document.getElementById("")
 var symptomInput = document.getElementById("inputSymptoms")
+var tags = [];
 
 // const getSymptoms = async sympRequest => {
 //   const symtoms = await fetch("url");
@@ -9,18 +10,48 @@ var symptomInput = document.getElementById("inputSymptoms")
 // }
 var symptomDataBase;
 var symptomsId = []
+loggingToken();
+function loggingToken(){
+    var uri = "https://sandbox-authservice.priaid.ch/login";
+    var userName = "jcide22@gmail.com"
+    var secret_key = "Nb82FwEm45Dsr6YAk";
+    var computedHash = CryptoJS.HmacMD5(uri, secret_key);
+    var computedHashString = computedHash.toString(CryptoJS.enc.Base64); 
+    var bearer = userName+":"+computedHashString
+    
+
+    var tokenCall = new XMLHttpRequest();
+    tokenCall.open('POST', uri, true);
+    tokenCall.setRequestHeader('Authorization','Bearer ' + bearer);
+    tokenCall.send();
+
+    tokenCall.onreadystatechange = function (){
+        if (this.readyState == 4 && this.status == 200) {
+			//callb(JSON.parse(this.responseText));
+           console.log(JSON.parse(this.responseText));
+    }
+}
+}
 
 
 function getSymptoms(callb) {
 	var sympRequest = new XMLHttpRequest();
 
-	sympRequest.open("GET", "https://sandbox-healthservice.priaid.ch/symptoms?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImpjaWRlMjJAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI2ODQyIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMjAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6Ijk5OTk5OTk5OSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IlByZW1pdW0iLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDIwLTA0LTE4IiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWF1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE1OTAxNzM0MTMsIm5iZiI6MTU5MDE2NjIxM30.v9hldwzBFFCiv-jhDQMDfuTXHvdRucVKFK6CE2NGtBI&format=json&language=en-gb");
+	sympRequest.open("GET", "https://sandbox-healthservice.priaid.ch/symptoms?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImpjaWRlMjJAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI2ODQyIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMjAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6Ijk5OTk5OTk5OSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IlByZW1pdW0iLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDIwLTA0LTE4IiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWF1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE1OTAyNDMwNjksIm5iZiI6MTU5MDIzNTg2OX0.Cxr3JaRykRe79lyBRW0u_TbgbT4SqjQRvWsQyR1SLb4&format=json&language=en-gb");
 	sympRequest.send();
 
 	sympRequest.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			callb(JSON.parse(this.responseText));
-			symptomDataBase = JSON.parse(this.responseText);
+            symptomDataBase = JSON.parse(this.responseText);
+            tags = symptomDataBase.map(x=>x.Name);
+            console.log(tags)
+            $( function() {
+
+            $( "#inputSymptoms" ).autocomplete({
+            source: tags
+            });
+            } );
 			//return symptomDataBase
 		}
 	};
@@ -64,9 +95,5 @@ symptomInput.addEventListener("keypress", function(e){
 
 
 })
-$( function() { 
-    symptomDataBase 
-    $( "#inputSymptoms" ).autocomplete({ 
-      source: tags 
-    }); 
-  } ); 
+
+
