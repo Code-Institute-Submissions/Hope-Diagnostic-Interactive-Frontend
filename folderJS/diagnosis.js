0a67b6a3acc6f5efd773b74f9fa07d57fabe98d0
SegzAyo <jcide22@gmail.com>
2020-05-24 18:05:45 +0000

    automatic token generation added

--- folderJS/diagnosis.js
+++ folderJS/diagnosis.js
@@ -2,6 +2,8 @@ var name = document.getElementById("")
 var yearOfBirth = document.getElementById("")
 var sex = document.getElementById("")
 var symptomInput = document.getElementById("inputSymptoms")
+var userLoggedSymptoms = []
+var userLogged = document.getElementById("userLoggedInput");
 var tags = [];
 
 // const getSymptoms = async sympRequest => {
@@ -28,16 +30,20 @@ function loggingToken(){
     tokenCall.onreadystatechange = function (){
         if (this.readyState == 4 && this.status == 200) {
 			//callb(JSON.parse(this.responseText));
-           console.log(JSON.parse(this.responseText));
+           //console.log(JSON.parse(this.responseText));
+           localStorage.setItem("token",JSON.parse(this.responseText).Token);
+           getSymptoms(logSymptoms)
+
     }
 }
 }
 
 
 function getSymptoms(callb) {
-	var sympRequest = new XMLHttpRequest();
-
-	sympRequest.open("GET", "https://sandbox-healthservice.priaid.ch/symptoms?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImpjaWRlMjJAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI2ODQyIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMjAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6Ijk5OTk5OTk5OSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IlByZW1pdW0iLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDIwLTA0LTE4IiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWF1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE1OTAyNDMwNjksIm5iZiI6MTU5MDIzNTg2OX0.Cxr3JaRykRe79lyBRW0u_TbgbT4SqjQRvWsQyR1SLb4&format=json&language=en-gb");
+    var sympRequest = new XMLHttpRequest();
+    var token = localStorage.getItem("token");
+    //console.log(token)
+	sympRequest.open("GET", "https://sandbox-healthservice.priaid.ch/symptoms?token="+token+"&format=json&language=en-gb");
 	sympRequest.send();
 
 	sympRequest.onreadystatechange = function() {
@@ -45,7 +51,7 @@ function getSymptoms(callb) {
 			callb(JSON.parse(this.responseText));
             symptomDataBase = JSON.parse(this.responseText);
             tags = symptomDataBase.map(x=>x.Name);
-            console.log(tags)
+            //console.log(tags)
             $( function() {
 
             $( "#inputSymptoms" ).autocomplete({
@@ -61,7 +67,6 @@ function logSymptoms(data) {
 	console.log(data);
 }
 
-getSymptoms(logSymptoms)
 
 
 
@@ -75,6 +80,8 @@ symptomInput.addEventListener("keypress", function(e){
   //}
 
 
+
+
   if(e.key == "Enter"){
       e.preventDefault();
       
@@ -85,10 +92,16 @@ symptomInput.addEventListener("keypress", function(e){
           //return symptomDataBase[i].Name && symptomsId.push(symptomDataBase[i].ID)
         console.log("Name "+symptomDataBase[i].Name+" Id"+symptomDataBase[i].ID)
         symptomsId.push(symptomDataBase[i].ID)
+        userLoggedSymptoms.push(symptomDataBase[i].Name)
+        var userLoggedHTML = '';
+        for (var i = 0; i < userLoggedSymptoms.length; i++) {
+        userLoggedHTML += '<span class="test">' + userLoggedSymptoms[i] + '</span><br/><br/>';
+        }
         }
     }
     document.getElementById("inputSymptoms").value = ""
     console.log(symptomsId)
+    userLogged.innerHTML = userLoggedHTML
 
 }
 
