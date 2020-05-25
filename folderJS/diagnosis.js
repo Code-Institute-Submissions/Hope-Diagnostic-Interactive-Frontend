450d08854cd1e5dc5526fccb993249ac1442b3b0
SegzAyo <jcide22@gmail.com>
2020-05-25 16:42:26 +0000

    user symptoms logged to page: diagnosis.js

--- folderJS/diagnosis.js
+++ folderJS/diagnosis.js
@@ -2,8 +2,8 @@ var name = document.getElementById("")
 var yearOfBirth = document.getElementById("")
 var sex = document.getElementById("")
 var symptomInput = document.getElementById("inputSymptoms")
-var userLoggedSymptoms = []
-var userLogged = document.getElementById("userLoggedInput");
+//var userLoggedSymptoms = []
+//var userLogged = document.getElementById("userLoggedInput");
 var tags = [];
 
 // const getSymptoms = async sympRequest => {
@@ -12,6 +12,8 @@ var tags = [];
 // }
 var symptomDataBase;
 var symptomsId = []
+//console.log(symptomsId)
+
 loggingToken();
 function loggingToken(){
     var uri = "https://sandbox-authservice.priaid.ch/login";
@@ -79,9 +81,6 @@ symptomInput.addEventListener("keypress", function(e){
   //  console.log(symptomFilterArray);
   //}
 
-
-
-
   if(e.key == "Enter"){
       e.preventDefault();
       
@@ -90,23 +89,31 @@ symptomInput.addEventListener("keypress", function(e){
       for (i = 0; i < symptomDataBase.length; i++) {
         if (symptomDataBase[i].Name.toLowerCase() === symptomInputValue.toLowerCase()) {
           //return symptomDataBase[i].Name && symptomsId.push(symptomDataBase[i].ID)
-        console.log("Name "+symptomDataBase[i].Name+" Id"+symptomDataBase[i].ID)
+        //console.log("Name "+symptomDataBase[i].Name+" Id"+symptomDataBase[i].ID)
         symptomsId.push(symptomDataBase[i].ID)
-        userLoggedSymptoms.push(symptomDataBase[i].Name)
-        var userLoggedHTML = '';
-        for (var i = 0; i < userLoggedSymptoms.length; i++) {
-        userLoggedHTML += '<span class="test">' + userLoggedSymptoms[i] + '</span><br/><br/>';
+        //userLoggedSymptoms.push(symptomDataBase[i].Name)
+            $("#userLoggedInput").append("<p>"+symptomDataBase[i].Name+"</p>")
+
+        
+        //var userLoggedHTML = '';
+        //for (var i = 0; i < userLoggedSymptoms.length; i++) {
+        //userLoggedHTML += '<span class="test">' + userLoggedSymptoms[i] + '</span><br/><br/>';
+        //}
         }
-        }
+        
     }
+
+
     document.getElementById("inputSymptoms").value = ""
     console.log(symptomsId)
-    userLogged.innerHTML = userLoggedHTML
+    //userLogged.innerHTML = userLoggedHTML
 
-}
-
-
-
+    }
 })
+
+//$(document).ready(function(){
+ //           $("userLoggedInput").append("<p>"+symptomDataBase[i].Name+"</p>")
+ //       })
+
 
 
