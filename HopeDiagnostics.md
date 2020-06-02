4022f0b666f8aab1883adce9e96ac67a1a13b205
SegzAyo <jcide22@gmail.com>
2020-06-02 18:41:39 +0000

    Final commit before submission

--- HopeDiagnostics.md
+++ HopeDiagnostics.md
@@ -1,4 +1,4 @@
-![alt text](assets/images/logo_transparent.png)
+![alt text](assets/images/Banner.png)
 Hope Diagnostics is an online symptom checker web application designed and built 
 by Segun Ayodele as the second milestone project at Code Institute. Hope Diagnostics 
 is primarily for patients who cannot access the regular medical services because of 
@@ -28,12 +28,11 @@ Andriana is a single mum of 5 year old son, she works works as a sue chef in a
 restuarant.
 Andriana has been mildly symptomatic and she resumes her next shift at work in 24 
 hours. As a Hope Diagnostics user I want/need/expect to:
-* Carry out diagnosis on my symptoms to know the severity of my condition there by informing my next line of action such as visit the hospital or self-isolate.
+* Carry out diagnosis on my symptoms to know the severity of my condition there by informing my next line of actions such as going to the hospital or self-isolate.
 * Find out the illness my symtoms are related to.
-* Schedule a call session with Hope Diagnostics doctors for cross examination and advice.
+* Schedule a call session with Hope Diagnostics doctors for further cross examination and advice.
 
-She wants to find out if her symptoms are stress related or something serious
-that may require a visit to the hopital or isolating herself to protect son and others.
+
 
 ### Design Choice
 
@@ -45,12 +44,27 @@ that may require a visit to the hopital or isolating herself to protect son and 
 ![alt text](assets/images/Hope-Diagnostics-colour-profile.png)
 
 **WireFrame**
+* Mobile view
+![alt text](assets/wireframe/Mobile-view.png)
+![alt text](assets/wireframe/Home-bg.png)
+![alt text](assets/wireframe/Diagnosis-bg.png)
 
 
 
 
 ## Features
-
+### Existing Features
+* __Navbar__ is featured on all the pages and contains the menu-links for navigation around the website.
+![alt text](assets/images/Navbar.png)
+* __Footer__ also featured on every page and contains the social media icons and navigation links around the website.
+![alt text](assets/images/footer.png)
+* __Home page__ is the landing page with medically inclined image and a short pitch of the diagnostic web application 
+with buttons to schedule a call session with doctors and the other links to the diagnosis page.
+The home also features how the web application works and images of the leading doctors of Hope Diagnostics.
+![alt text](assets/images/Home-screen.png) ![alt text](assets/images/Mobile-home.png)
+* __Diagnosis page__ features the instruction on how to use the diagnosis system and right below is the patient's 
+data input point for the diagnosis. Also on the this page is navbar and the footer.
+![alt text](assets/images/Diagnosis-bg.png) ![alt text](assets/images/Diagnosis-mobile.png)
 
 
 ## Technology Used
@@ -59,19 +73,74 @@ that may require a visit to the hopital or isolating herself to protect son and 
 * __Adobe Illustrator__ used for wireframe design.
 * __Adobe Photoshop__ used for image resize and compression.
 * __Git__ for version control
+* __Coolors__ for color palette generation
+
 
 ### Libraries
-* [Bootstrap](https://getbootstrap.com/)
-* [Google Fonts](https://fonts.google.com/)
-* [jQuery](https://jquery.com/)
-* [Font Awasome](https://fontawesome.com/)
+* [Bootstrap](https://getbootstrap.com/) To simplify the responsiveness and the structure of the website.
+* [Google Fonts](https://fonts.google.com/) for font styling.
+* [jQuery](https://jquery.com/) To simplify DOM manipulation.
+* [Font Awasome](https://fontawesome.com/) To incorporate icons.
+* [Apimedic API](https://apimedic.com/news) Provides the data for the web application.
 
 ### Programming Languages
 * HTML
 * CSS 
 * Javascript
 
+## Test
+The following tools were used to validate and beautify the codes:
+* [Freeformatter](https://www.freeformatter.com/html-formatter.html#ad-output) used to beautify the HTML code.
+* [BeautyTools](http://beautifytools.com/css-validator.php) used to beautify and validate the Javascript and CSS codes.
+
+The navigation of this website has been tested, and every button works with the exception of the social media icon and the About Us page.
+The website responsiveness has also been tested at all screen sizes and it adapts and displays properly.
+The test carried out on the functionality of the diagnosis web application includes:
+* XMLHttpRequest and Response for symptom JSON.
+* User Input Iteration through the symptom JSON.
+* User symptom input autocomplete through iteration of symptom JSON
+* Symptom ID appended into an array from Iteration result.
+* Token auto generator request.
+* User is alerted on symptom typo error.
+* XMLHttpRequest and Response for diagnosis JSON.
+* Diagnosis result returned and displayed on submission.
+
+The above listed functionalities were tested and works as expected.
+
+To test the diagnosis web app, use the following combinations of symptoms:
+* __Test 1__ 
+    * Year of birth:1995 
+    * Sex: female
+    * Symptoms: abdominal pain, fever
+
+* __Test 2__
+    * Year of birth:1990
+    * Sex: male
+    * Symptoms: fever, headache, abdominal pain.
+
+* __Test 3__
+    * Year of birth:1992
+    * Sex: male
+    * Symptoms: back pain, abdominal pain.    
+
+
+### Limitations
+* The API endpoint used in this project is the Sandbox, which returns a dummy 
+diagnostic data. I decided use the Sandbox because the live API is often offline.
+* The clear button only clears the logged symptoms and Diagnosis 
+results, the year of birth and sex remains until page reloads or manually removed.
+* when symptom combinations returns no diagnosis result, there is no message to 
+alert the user.
+* Because of time constraint, the schedule generator API could not be integrated 
+into the website, instead the schedule button links an external schedule generator service.
+* The About us page was not designed
+
+
 ## Deployment
+Hope Diagnostics is deployed and hosted on github pages. 
+From the repository, I clicked setting then selected a theme under the section of github 
+pages. I then wrote about Curryt website then commit the changes. Finally, I added my repository 
+name for it to be searchable.
 
 
 ## Credits
@@ -86,12 +155,14 @@ that may require a visit to the hopital or isolating herself to protect son and 
 
 ## Acknowledgement
 
-I recieved inspiration and references from the following symptom checker 
-web applications:
+I recieved inspiration and references from the following sources:
 * [Healthcare Direct](https://www.healthdirect.gov.au/symptom-checker)
 * [Mayo Clinic](https://www.mayoclinic.org/)
 * [WebMD](https://symptoms.webmd.com/default.htm)
 * [Isabel](https://symptomchecker.isabelhealthcare.com/)
+* [Quick Autocomplete App With JS & JSON](https://www.youtube.com/watch?v=1iysNUrI3lw)
+* [JavaScript Higher Order Functions & Arrays](https://www.youtube.com/watch?v=rRgD1yVwIvE)
+* [Async JS Crash Course - Callbacks, Promises, Async Await](https://www.youtube.com/watch?v=PoRJizFvM7s)
 
 
 
--- assets/folderJS/diagnosis.js
+++ assets/folderJS/diagnosis.js
@@ -1,67 +1,47 @@
-//var name = document.getElementById("")
+/*---------------------------------------USER_DATA_INPUT--*/
 var yearOfBirth = document.getElementById("yearOfBirthInput")
 var sex = document.getElementsByName("customRadioInline1")
 var symptomInput = document.getElementById("inputSymptoms")
 var diagnosisData;
-//var userLoggedSymptoms = []
-//var userLogged = document.getElementById("userLoggedInput");
 var tags = [];
-
-// const getSymptoms = async sympRequest => {
-//   const symtoms = await fetch("url");
-//   const symtomResponse= await symtoms.json();
-// }
 var symptomDataBase;
 var symptomsId = []
-//console.log(symptomsId)
-
 loggingToken();
-function loggingToken(){
-    var uri = "https://sandbox-authservice.priaid.ch/login";
-    var userName = "jcide22@gmail.com"
-    var secret_key = "Nb82FwEm45Dsr6YAk";
-    var computedHash = CryptoJS.HmacMD5(uri, secret_key);
-    var computedHashString = computedHash.toString(CryptoJS.enc.Base64); 
-    var bearer = userName+":"+computedHashString
-    
-
-    var tokenCall = new XMLHttpRequest();
-    tokenCall.open('POST', uri, true);
-    tokenCall.setRequestHeader('Authorization','Bearer ' + bearer);
-    tokenCall.send();
-
-    tokenCall.onreadystatechange = function (){
-        if (this.readyState == 4 && this.status == 200) {
-			//callb(JSON.parse(this.responseText));
-           //console.log(JSON.parse(this.responseText));
-           localStorage.setItem("token",JSON.parse(this.responseText).Token);
-           getSymptoms(logSymptoms)
-
-    }
+/*-------------------------------------API_TOKEN_AUTO_GENERATOR--*/
+function loggingToken() {
+	var uri = "https://sandbox-authservice.priaid.ch/login";
+	var userName = "jcide22@gmail.com"
+	var secret_key = "Nb82FwEm45Dsr6YAk";
+	var computedHash = CryptoJS.HmacMD5(uri, secret_key);
+	var computedHashString = computedHash.toString(CryptoJS.enc.Base64);
+	var bearer = userName + ":" + computedHashString
+	var tokenCall = new XMLHttpRequest();
+	tokenCall.open('POST', uri, true);
+	tokenCall.setRequestHeader('Authorization', 'Bearer ' + bearer);
+	tokenCall.send();
+	tokenCall.onreadystatechange = function() {
+		if(this.readyState == 4 && this.status == 200) {
+			localStorage.setItem("token", JSON.parse(this.responseText).Token);
+			getSymptoms(logSymptoms)
+		}
+	}
 }
-}
-
-
+/*-----------------------------------------SYMPTOMS_XMLHTTP_REQUEST--*/
 function getSymptoms(callb) {
-    var sympRequest = new XMLHttpRequest();
-    var token = localStorage.getItem("token");
-    //console.log(token)
-	sympRequest.open("GET", "https://sandbox-healthservice.priaid.ch/symptoms?token="+token+"&format=json&language=en-gb");
+	var sympRequest = new XMLHttpRequest();
+	var token = localStorage.getItem("token");
+	sympRequest.open("GET", "https://sandbox-healthservice.priaid.ch/symptoms?token=" + token + "&format=json&language=en-gb");
 	sympRequest.send();
-
 	sympRequest.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
+		if(this.readyState == 4 && this.status == 200) {
 			callb(JSON.parse(this.responseText));
-            symptomDataBase = JSON.parse(this.responseText);
-            tags = symptomDataBase.map(x=>x.Name);
-            //console.log(tags)
-            $( function() {
-
-            $( "#inputSymptoms" ).autocomplete({
-            source: tags
-            });
-            } );
-			//return symptomDataBase
+			symptomDataBase = JSON.parse(this.responseText);
+			tags = symptomDataBase.map(x => x.Name);
+			$(function() {
+				$("#inputSymptoms").autocomplete({
+					source: tags
+				});
+			});
 		}
 	};
 }
@@ -69,102 +49,64 @@ function getSymptoms(callb) {
 function logSymptoms(data) {
 	console.log(data);
 }
-
-
-
-
-symptomInput.addEventListener("keypress", function(e){
-
-  //symptomFilterArray = []
-  //if(e.target.value){
-  //  symptomFilterArray = symptomDataBase.filter(symptomDataBase => symptomDataBase.ToLowerCase().includes(e.target.value));
-  //  symptomFilterArray = symptomDataBase.map()
-  //  console.log(symptomFilterArray);
-  //}
-
-  if(e.key == "Enter"){
-      e.preventDefault();
-      
-    var symptomInputValue = document.getElementById("inputSymptoms").value
-      var spellCheck = false; 
-      for (i = 0; i < symptomDataBase.length; i++) {
-        if (symptomDataBase[i].Name.toLowerCase() === symptomInputValue.toLowerCase()) {
-          //return symptomDataBase[i].Name && symptomsId.push(symptomDataBase[i].ID)
-        //console.log("Name "+symptomDataBase[i].Name+" Id"+symptomDataBase[i].ID)
-        symptomsId.push(symptomDataBase[i].ID)
-        //userLoggedSymptoms.push(symptomDataBase[i].Name)
-            $("#userLoggedInput").append("<p>"+symptomDataBase[i].Name+"</p>")
-
-        spellCheck = true;
-        //var userLoggedHTML = '';
-        //for (var i = 0; i < userLoggedSymptoms.length; i++) {
-        //userLoggedHTML += '<span class="test">' + userLoggedSymptoms[i] + '</span><br/><br/>';
-        //}
+/*---------------------------------------------SYMPTOM_INPUT_ITERATION--*/
+symptomInput.addEventListener("keypress", function(e) {
+	if(e.key == "Enter") {
+		e.preventDefault();
+		var symptomInputValue = document.getElementById("inputSymptoms").value
+		var spellCheck = false;
+		for(i = 0; i < symptomDataBase.length; i++) {
+			if(symptomDataBase[i].Name.toLowerCase() === symptomInputValue.toLowerCase()) {
+				symptomsId.push(symptomDataBase[i].ID)
+				$("#userLoggedInput").append("<p>" + symptomDataBase[i].Name + "</p>")
+				spellCheck = true;
+			}
         }
-
-        
-    }
-    if (!spellCheck){
-        alert("Check your spellings and Enter correct symptom")
-    }
-
-
-    document.getElementById("inputSymptoms").value = ""
-    console.log(symptomsId)
-    //userLogged.innerHTML = userLoggedHTML
-
-    }
+/*------------------------------------------------SYMPTOM_INPUT_ERROR_ALAERT--*/        
+		if(!spellCheck) {
+			alert("Check your spellings and Enter correct symptom")
+		}
+		document.getElementById("inputSymptoms").value = ""
+		console.log(symptomsId)
+	}
 })
 
-//$(document).ready(function(){
- //           $("userLoggedInput").append("<p>"+symptomDataBase[i].Name+"</p>")
- //       })
-
-
-
 function getDiagnosis(event) {
-    var gender;
-        for(i = 0; i < sex.length; i++) {
-        if(sex[i].checked){
-            gender = sex[i].value;
-        
-        }
-        
-        }
-    console.log(yearOfBirth.value)
-    event.preventDefault();
-    var sympRequest = new XMLHttpRequest();
-    var token = localStorage.getItem("token");
-    //console.log(token)
-	sympRequest.open("GET", "https://sandbox-healthservice.priaid.ch/diagnosis?symptoms="+JSON.stringify(symptomsId)+"&gender="+gender+"&year_of_birth="+yearOfBirth.value+"&token="+token+"&format=json&language=en-gb");
+	var gender;
+	for(i = 0; i < sex.length; i++) {
+		if(sex[i].checked) {
+			gender = sex[i].value;
+		}
+    }
+    
+/*-----------------------------------------------------DIAGNOSIS_XMLHTTP_REQUEST--*/    
+	console.log(yearOfBirth.value)
+	event.preventDefault();
+	var sympRequest = new XMLHttpRequest();
+	var token = localStorage.getItem("token");
+	sympRequest.open("GET", "https://sandbox-healthservice.priaid.ch/diagnosis?symptoms=" + JSON.stringify(symptomsId) + "&gender=" + gender + "&year_of_birth=" + yearOfBirth.value + "&token=" + token + "&format=json&language=en-gb");
 	sympRequest.send();
-
 	sympRequest.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-            //callb(JSON.parse(this.responseText));
-            
-            diagnosisData = JSON.parse(this.responseText);
-            //tags = symptomDataBase.map(x=>x.Name);
-            //console.log(tags)
-            console.log(diagnosisData)
-            showDiagnosis(diagnosisData)
-			//return symptomDataBase
+		if(this.readyState == 4 && this.status == 200) {
+			diagnosisData = JSON.parse(this.responseText);
+			console.log(diagnosisData)
+			showDiagnosis(diagnosisData)
 		}
 	};
 }
-
-function showDiagnosis(data){
-    for(i = 0; i < data.length; i++) {
-        $("#returnedDiagnosis").append("<p> Name: "+data[i].Issue.Name+"</p>");
-        $("#returnedDiagnosis").append("<p> Accuracy: "+data[i].Issue.Accuracy+"</p>");
-        $("#returnedDiagnosis").append("<p> Professional Name: "+data[i].Issue.ProfName+"</p>");
-        $("#returnedDiagnosis").append("<br>");
-    }
+/*--------------------------------------------RESULT_LOGGED_TO_PAGE--*/
+function showDiagnosis(data) {
+	for(i = 0; i < data.length; i++) {
+		$("#returnedDiagnosis").append("<p> Name: " + data[i].Issue.Name + "</p>");
+		$("#returnedDiagnosis").append("<p> Accuracy: " + data[i].Issue.Accuracy + "</p>");
+		$("#returnedDiagnosis").append("<p> Professional Name: " + data[i].Issue.ProfName + "</p>");
+		$("#returnedDiagnosis").append("<br>");
+	}
 }
-
-function clearBoxes(event){
-    event.preventDefault()
-    $("#userLoggedInput").find('p').remove()
+/*-------------------------------------------------CLEAR_BOXES--*/
+function clearBoxes(event) {
+	event.preventDefault()
+	$("#userLoggedInput").find('p').remove()
     $("#returnedDiagnosis").find('p').remove()
+    
 }
-
